44f7e96e22014cb6883f19b4fe7217011ed5924b
chapter1617 <goyal.mansi16@gmail.com>
2024-02-15 10:27:20 -0800

    clear ui state

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+
 
 function addItem(e){
     e.preventDefault();
@@ -23,6 +25,8 @@ function addItem(e){
 
     itemList.appendChild(li);
 
+    checkUI();
+
     itemInput.value = "";
 }
 
@@ -42,13 +46,30 @@ function createIcon(classes) {
 
 function removeItem (e){
     if(e.target.parentElement.classList.contains('remove-item')){
+       if(confirm('Are you sure?')){
         e.target.parentElement.parentElement.remove();
+        checkUI();
+       }
+        
     }
 }
 
 function clearItems(){
     while(itemList.firstChild){
         itemList.removeChild(itemList.firstChild);
+    }
+    checkUI();
+}
+
+function checkUI(){
+    const items = itemList.querySelectorAll('li');
+
+    if(items.length === 0){
+        clearBtn.style.display = 'none';
+        itemFilter.style.display = 'none';
+    }else{
+        clearBtn.style.display = 'block';
+        itemFilter.style.display = 'block';
     }
 }
 
@@ -57,3 +78,5 @@ function clearItems(){
 itemForm.addEventListener('submit',addItem);
 itemList.addEventListener('click',removeItem)
 clearBtn.addEventListener('click',clearItems);
+
+checkUI();
